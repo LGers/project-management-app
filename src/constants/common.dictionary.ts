fe1cb767f0f325937cc5759560fd176de4e814aa
LGers <87089817+LGers@users.noreply.github.com>
2022-05-04 20:25:12 +0300

    fix: delete default css. feat: add mui/styled to package

--- src/constants/common.dictionary.ts
+++ src/constants/common.dictionary.ts
@@ -4,5 +4,3 @@ export const PATH = {
 
 export const WELCOME_PAGE_TITLE = 'Welcome to Lemasello';
 export const PAGE_404_TITLE = 'Error 404, this page can not be found...';
-
-export const PAGINATOR_LIMIT = 20;
