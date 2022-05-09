6c6cd918b8481f259ecb6d6084e9777ebd7d77c2
LGers <87089817+LGers@users.noreply.github.com>
2022-05-09 22:10:09 +0300

    fix: protected routes/ redirect to main page after Login

--- src/App.tsx
+++ src/App.tsx
@@ -19,29 +19,9 @@ const ProtectedRoute = ({ isAuth }: { isAuth: boolean }): ReactJSXElement => {
   if (!isAuth) {
     return <Navigate to={PATH.HOME} replace />;
   }
-  return <WelcomePage />;
+  return <MainPage />;
 };
 
-const ProtectedRouteWithChildren = ({ isAuth, children }: ProtectedRouteProps): ReactJSXElement => {
-  if (!isAuth) {
-    return <Navigate to={PATH.HOME} replace />;
-  }
-
-  return children;
-};
-
-const PrRoute = ({ path, isAuth }: { path: string; isAuth: boolean }) => {
-  return (
-    <Route
-      path={path}
-      element={
-        <ProtectedRouteWithChildren isAuth={isAuth}>
-          <MainPage />
-        </ProtectedRouteWithChildren>
-      }
-    />
-  );
-};
 const homePage = (isAuth: boolean): ReactJSXElement => {
   if (isAuth) return <MainPage />;
   return <WelcomePage />;
@@ -56,50 +36,13 @@ export const App = () => {
         <Route path={PATH.HOME} element={homePage(auth.isAuth)} />
         <Route path={PATH.LOG_IN} element={<Auth formName={'signIn'} />} />
         <Route path={PATH.SIGN_UP} element={<Auth formName={'signUp'} />} />
-        {/*<Route
-          path={PATH.HOME}
-          element={
-            <ProtectedRoute isAuth={true}>
-              <>
-                <Route path={PATH.HOME} element={<MainPage />} />
-                <Route path={PATH.COLUMNS} element={<MainPage />} />
-                <Route path={PATH.BOARDS} element={<MainPage />} />
-                <Route path={PATH.PROFILE} element={<MainPage />} />
-              </>
-            </ProtectedRoute>
-          }
-        />*/}
-        {/*<PrRoute path={PATH.COLUMNS} isAuth={auth.isAuth} />*/}
         <Route element={<ProtectedRoute isAuth={auth.isAuth} />}>
           <Route path={PATH.HOME} element={<MainPage />} />
           <Route path={PATH.COLUMNS} element={<MainPage />} />
           <Route path={PATH.BOARDS} element={<MainPage />} />
           <Route path={PATH.PROFILE} element={<MainPage />} />
         </Route>
-        {/*<Route
-          path={PATH.COLUMNS}
-          element={
-            <ProtectedRouteWithChildren isAuth={auth.isAuth}>
-              <MainPage />
-            </ProtectedRouteWithChildren>
-          }
-        />
-        <Route
-          path={PATH.BOARDS}
-          element={
-            <ProtectedRouteWithChildren isAuth={auth.isAuth}>
-              <MainPage />
-            </ProtectedRouteWithChildren>
-          }
-        />
-        <Route
-          path={PATH.PROFILE}
-          element={
-            <ProtectedRouteWithChildren isAuth={true}>
-              <MainPage />
-            </ProtectedRouteWithChildren>
-          }
-        />*/}
+
         <Route path="*" element={<Page404 />} />
       </Routes>
     </BrowserRouter>
