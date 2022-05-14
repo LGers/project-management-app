34642b2fed10a70aaf60c5d639b66d988c123e1d
LGers <87089817+LGers@users.noreply.github.com>
2022-05-14 20:56:35 +0300

    fix: conflict dictionary

--- src/i18n/dictionary.ru.ts
+++ src/i18n/dictionary.ru.ts
@@ -1,10 +1,7 @@
 import { welcomePageRu } from './welcomePage.dictionary';
 import { authRu } from './auth.dictionary';
-<<<<<<< HEAD
 import { mainPageRu } from './mainPage.dictionary';
-=======
 import { page404Ru } from './page404.dictionary';
->>>>>>> 00f7800b4a5b5402171ffe6a18ef3896c80d29d5
 
 export const ruTranslation = {
   languageLabel: 'Язык',
