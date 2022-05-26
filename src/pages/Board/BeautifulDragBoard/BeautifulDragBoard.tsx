8485ffb3f65327196ae629f0f060966c7e835e31
LGers <87089817+LGers@users.noreply.github.com>
2022-05-26 11:23:30 +0300

    init: BeautifulDragBoard

---
+++ src/pages/Board/BeautifulDragBoard/BeautifulDragBoard.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+type Props = {
+  s: string;
+};
+export const BeautifulDragBoard = (props: Props) => {
+  return (
+    <div>
+      <p>BeautifulDragBoard</p>
+    </div>
+  );
+};
